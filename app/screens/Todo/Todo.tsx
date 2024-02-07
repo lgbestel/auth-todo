48592c95ea6c2411b02a6a6c9261695103d74882
lgbestel <luizgbes@gmail.com>
2024-02-07 20:02:49 -0300

    feat: implement logout button

--- app/screens/Todo/Todo.tsx
+++ app/screens/Todo/Todo.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Button } from './styles';
+import { firebaseAuth } from '../../../firebaseConfig';
 
 const Todo = () => {
+  const handleLogout = () => firebaseAuth.signOut();
+
   return (
     <View>
       <Text>Todo</Text>
+      <Button title='Logout' onPress={handleLogout}/>
     </View>
   )
 }
---
+++ app/screens/Todo/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Button = styled.Button`
+  background-color: light-blue;
+  border-radius: 30px;
+  width: 50%;
+`
