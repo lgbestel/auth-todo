dfecd82182e9de07120694400c0090db3659754f
lgbestel <luizgbes@gmail.com>
2024-02-08 20:20:06 -0300

    feat: add firestore

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7jyWuWZMFR0HRvpDYYQTN2E8PXHEeuWc",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
+export const fireStore = getFirestore(firebaseApp);
