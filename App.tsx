01f41d2b3eb5950e2db8dfbec529998ca77c5b0d
lgbestel <luizgbes@gmail.com>
2024-02-13 23:21:49 -0300

    fix: remove redux hooks from app as theyre outside provider

--- App.tsx
+++ App.tsx
@@ -3,34 +3,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './app/screens/Login/Login';
 import Todo from './app/screens/Todo/Todo';
-import { User, onAuthStateChanged } from 'firebase/auth';
-import { useEffect, useState } from 'react';
-import { firebaseAuth } from './firebaseConfig';
-import { useAppDispatch, useAppSelector } from './app/store/hooks';
-import authSlice from './app/store/slices/authSlice';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Login: undefined,
+  Todo: undefined,
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector((state) => state.auth.user)
-
-  useEffect(() => {
-    onAuthStateChanged(firebaseAuth, (user) => {
-      console.log('User email: ', user?.email);
-      dispatch(authSlice.actions.setUser(user));
-    })
-  }, [])
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        {
-          user ?
-            <Stack.Screen name='Todo' component={Todo} options={{ headerShown: false }} />
-            : <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-        }        
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Todo'>
+          <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />        
+          <Stack.Screen name='Todo' component={Todo} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
